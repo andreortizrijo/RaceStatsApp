b669ca69e01a0b57cce7d5b132918f48e2d06fa3
André Rijo <andreortizrijo@gmail.com>
2020-11-20 13:42:52 +0000

    UI Added

--- RaceStatsApp.py
+++ RaceStatsApp.py
@@ -5,6 +5,7 @@ import sys
 
 #---VAR INITIALIZATION---#
 getcar = Car()
+laps = 0
 lapCount = 0
 l_lapcount = 0
 l_speed = 0
@@ -19,14 +20,14 @@ a_gear = []
 
 #---Main Code---#
 def acMain(ac_version):
-    global l_lapcount, l_speed, l_rpm, l_gear
+    global l_lapcount, l_speed, l_rpm, l_gear, laps, lapCount
 
     #---UI---#
     appWindow = ac.newApp("Race Stats")
     
-    ac.setSize(appWindow, 200, 400)
+    ac.setSize(appWindow, 200, 300)
 
-    l_lapcount = ac.addLabel(appWindow, "Laps: 0")
+    l_lapcount = ac.addLabel(appWindow, "Lap: 1")
     ac.setPosition(l_lapcount, 3, 30)
     l_speed = ac.addLabel(appWindow, "KMH: 0")
     ac.setPosition(l_speed, 3, 50)
@@ -39,15 +40,15 @@ def acMain(ac_version):
 
 #---Updated Values---#
 def acUpdate(deltaT):
-    global lapCount, l_speed, l_rpm, l_gear, a_speedMS, a_speedMPH, a_speedKMH, a_rpm, a_gear
+    global laps, lapCount, l_lapcount, l_speed, l_rpm, l_gear, a_speedMS, a_speedMPH, a_speedKMH, a_rpm, a_gear
 
-    laps = ac.getCarState(0, acsys.CS.LapCount)
+    laps = ac.getCarState(0, acsys.CS.LapCount) + 1
 
-    # a_speedMS.append(getcar.speedMS())
-    # a_speedMPH.append(getcar.speedMPH())
-    # a_speedKMH.append(getcar.speedKMH())
-    # a_rpm.append(getcar.rmp())
-    # a_gear.append(getcar.gear())
+    a_speedMS.append(getcar.speedMS())
+    a_speedMPH.append(getcar.speedMPH())
+    a_speedKMH.append(getcar.speedKMH())
+    a_rpm.append(getcar.rmp())
+    a_gear.append(getcar.gear())
 
     ac.setText(l_speed, "KMH: {}".format(getcar.speedKMH()))
     ac.setText(l_rpm, "RPM: {}".format(getcar.rmp()))
@@ -56,10 +57,9 @@ def acUpdate(deltaT):
     #---Get info when cross the lap---#
     if laps > lapCount:
         lapCount = laps
-        ac.setText(l_lapcount, "Laps: {}".format(lapcount))
-
-        # ac.log("Speed MS: {}".format(a_speedMS))
-        # ac.log("Speed MPH: {}".format(a_speedMPH))
-        # ac.log("Speed MKH: {}".format(a_speedKMH))
-        # ac.log("Speed RPM: {}".format(a_rpm))
-        # ac.log("Speed Gear: {}".format(a_gear))
+        ac.setText(l_lapcount, "Laps: {}".format(lapCount))
+        ac.log("Speed MS: {}".format(a_speedMS))
+        ac.log("Speed MPH: {}".format(a_speedMPH))
+        ac.log("Speed MKH: {}".format(a_speedKMH))
+        ac.log("Speed RPM: {}".format(a_rpm))
+        ac.log("Speed Gear: {}".format(a_gear))
