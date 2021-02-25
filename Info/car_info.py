af43992d6a4500e37473f8c7a49adda985eeb9ab
André Rijo <andreortizrijo@gmail.com>
2021-02-25 11:13:49 +0000

    Data structured complete

--- Info/car_info.py
+++ Info/car_info.py
@@ -1,7 +1,5 @@
-import ac, acsys
-import os, sys
+import ac, acsys, os, sys
 import platform
-import threading
 
 if platform.architecture()[0] == "64bit":
     libdir = 'third_party/lib64'
@@ -13,9 +11,6 @@ os.environ['PATH'] = os.environ['PATH'] + ";."
 from third_party.sim_info import info
 
 class Car():
-
-    #def getCarInfo(self, property):
-    #    return ac.getCarState(ID, property) 
 
     def getCarInfo(self, property):
         return property
--- Info/track_info.py
+++ Info/track_info.py
@@ -1,7 +1,5 @@
-import ac, acsys
-import os, sys
+import ac, acsys, os, sys
 import platform
-import threading
 
 if platform.architecture()[0] == "64bit":
     libdir = 'third_party/lib64'
@@ -14,29 +12,5 @@ from third_party.sim_info import info
 
 class Track():
 
-    def getTrackName(self):
-        trackname = info.static.track
-        if trackname:
-            trackname = trackname.replace('ks', '')
-            trackname = trackname.replace('_', '')
-        else:
-            trackname = 'No information about track name.'
-
-        return trackname
-    
-    def getTrackConfig(self):
-        trackconfig = info.static.trackConfiguration
-        if trackconfig:
-            trackconfig = trackconfig.replace('ks', '')
-            trackconfig = trackconfig.replace('_', '')
-        else:
-            trackconfig = 'No information about track configuration.'
-
-        return trackconfig
-
     def getTrackInfo(self, property):
         return property
-    
-    # The highest number = higher lvl of difficulty on curves
-    #def getTrackSPlineLength(self):
-        #return info.static.trackSPlineLength
