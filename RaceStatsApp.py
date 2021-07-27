a443a38931155dd8ef9d17ca18c3c1365f414312
André Rijo <andreortizrijo@gmail.com>
2021-07-27 10:56:22 +0100

    Added Checkbox and Event On Checkbox Change to retrieve data

--- RaceStatsApp.py
+++ RaceStatsApp.py
@@ -1,5 +1,5 @@
 import ac, acsys, os, sys
-import platform, socket, pickle, time
+import platform, socket, pickle
 
 if platform.architecture()[0] == "64bit":
     libdir = 'third_party/lib64'
@@ -10,30 +10,35 @@ os.environ['PATH'] = os.environ['PATH'] + ";."
 
 from configparser import ConfigParser
 from third_party.sim_info import info
-from array import array
 
 #---VAR INITIALIZATION---#
 ID = 0
+SESSION_FIRST = True
+IS_RACING = False
+BUFFER = []
+
+#---SHARED MEMORY API---#
+STATIC = info.static
+GRAPHICS = info.graphics
+PHYSICS = info.physics
+
+#---ROOT DIRECTORY---#
+PATH = '/'.join((os.path.abspath(__file__).replace('\\', '/')).split('/')[:-1])
+CONFIG_OBJECT = ConfigParser()
+CONFIG_OBJECT.read(os.path.join(PATH, 'config.ini'))
+
+#---SOCKET COMMUNICATION---#
 HEADER = 4096
 SERVER = '127.0.0.1'
 PORT = 8081
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = '!DISCONNECT'
-CONFIG_OBJECT = ConfigParser()
-FIRST_RUN = True
-BUFFER = []
-STATIC = info.static
-GRAPHICS = info.graphics
-PHYSICS = info.physics
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+#client.connect(ADDR)
 
-path = '/'.join((os.path.abspath(__file__).replace('\\', '/')).split('/')[:-1])
-CONFIG_OBJECT.read(os.path.join(path, 'config.ini'))
-
-def encodeData(data): 
+def dump_data(data): 
     message = pickle.dumps(data)
     message_buffer = message
     message = str(len(message)).encode(FORMAT)
@@ -41,32 +46,31 @@ def encodeData(data):
     
     return message
 
-def sendData(data, first_run):
+def send_data(data, is_racing):
     if data == DISCONNECT_MESSAGE:
-        message = encodeData(data)
+        message = dump_data(data)
         client.send(message)
-        return
 
     BUFFER.append(data)
 
-    if first_run == True:
-        message = encodeData(BUFFER)
+    if is_racing == True:
+        message = dump_data(BUFFER)
         client.send(message)
         BUFFER.clear()
 
-    if len(BUFFER) == 60:
-        message = encodeData(BUFFER)
+    if len(BUFFER) == 5:
+        message = dump_data(BUFFER)
         client.send(message)
         BUFFER.clear()
  
-def formatTime(time):
+def format_time(time):
     time_seconds = (time / 1000) % 60
     time_minutes = (time // 1000) // 60
     current_time = "{:.0f}:{:06.3f}".format(time_minutes, time_seconds)
 
     return current_time
 
-def setArray(array, type, length):
+def set_array(array, type, length):
     for i in range(length):
         array[i] = type[i]
 
@@ -74,18 +78,36 @@ def setArray(array, type, length):
 
 #---UI---#
 def acMain(ac_version):
+    appWindow = ac.newApp('RaceStats')
+    ac.setSize(appWindow, 280, 70)
+    ac.setBackgroundOpacity(appWindow, 0)
+
+    racing_checkbox = ac.addCheckBox(appWindow, 'TURN ON IF YOU ARE RACING!')
+    ac.setPosition(racing_checkbox, 3, 35)
+    ac.addOnCheckBoxChanged(racing_checkbox, racing_checkbox_onclick)
+
     return "Race Stats"
 
+def racing_checkbox_onclick(*args):
+    global IS_RACING
+
+    if IS_RACING:
+        IS_RACING = False
+    else:
+        IS_RACING = True
+
+#---RUNNING---#
 def acUpdate(deltaT):
-    global BUFFER, FIRST_RUN, STATIC, PHYSICS, GRAPHICS
+    global BUFFER, IS_RACING, SESSION_FIRST
+    global STATIC, PHYSICS, GRAPHICS
 
-    current_state = GRAPHICS.status
+    session_state = GRAPHICS.status
 
-    time = formatTime(abs(GRAPHICS.sessionTimeLeft))
-    best_lap = formatTime(ac.getCarState(ID, acsys.CS.BestLap))
+    time = format_time(abs(GRAPHICS.sessionTimeLeft))
+    best_lap = format_time(ac.getCarState(ID, acsys.CS.BestLap))
 
-    if FIRST_RUN:
-        data = {
+    if SESSION_FIRST:
+        session_data = {
             "METADATA":{
                 "TOKEN":CONFIG_OBJECT['AUTH']['token']
             },
@@ -95,44 +117,56 @@ def acUpdate(deltaT):
             }
         }
 
-        sendData(data, FIRST_RUN)
-        FIRST_RUN = False
+        #send_data(session_data, IS_RACING)
+        SESSION_FIRST = False
 
-    if current_state == 2:
-        if time != "nan:000nan":
-            data = {
-                "METADATA":{
-                    "TOKEN":CONFIG_OBJECT['AUTH']['token']
-                },
-                "TRACK_INFO":{
-                    "TRACK_SPLINE_LENGTH":str(STATIC.trackSPlineLength),
-                    "TRACK_SECTOR_COUNT":str(STATIC.sectorCount),
-                    "TRACK_AIR_DENSITY":str(PHYSICS.airDensity),
-                    "TRACK_AIR_TEMPERATURE":str(PHYSICS.airTemp),
-                    "TRACK_ROAD_TEMPERATURE":str(PHYSICS.roadTemp),
-                    "TRACK_WIND_SPEED":str(GRAPHICS.windSpeed),
-                    "TRACK_WIND_DIRECTION":str(GRAPHICS.windDirection),
-                    "TRACK_SURFACE_GRIP":str(GRAPHICS.surfaceGrip),
-                },
-                "CAR_INFO":{
-                    "CAR_MODEL":str(STATIC.carModel),
-                    "CAR_SPONSER":str(STATIC.carSkin),
-                    "CAR_SPEEDKMH":str(PHYSICS.speedKmh),
-                    "CAR_RPM":str(PHYSICS.rpms),
-                    "CAR_GEAR":str(PHYSICS.gear - 1),
-                    "CAR_GAS_PEDAL":str(PHYSICS.gas),
-                    "CAR_BRAKE_PEDAL":str(PHYSICS.brake),
-                    "CAR_CLUTCH_PEDAL":str(PHYSICS.clutch),
-                    "CAR_STEER_ANGLE":str(PHYSICS.steerAngle),
-                    "CAR_TYRE_COMPOUND":str(GRAPHICS.tyreCompound)
-                },
-                "TIME_INFO":{
-                    "TIME_CURRENT_TIME":str(time),
-                    "TIME_BEST_TIME":str(best_lap)
-                }
+    if IS_RACING:
+        track_data = {
+            "METADATA":{
+                "TOKEN":CONFIG_OBJECT['AUTH']['token']
+            },
+            "TRACK_INFO":{
+                "TRACK_SPLINE_LENGTH":str(STATIC.trackSPlineLength),
+                "TRACK_SECTOR_COUNT":str(STATIC.sectorCount),
+                "TRACK_AIR_DENSITY":str(PHYSICS.airDensity),
+                "TRACK_AIR_TEMPERATURE":str(PHYSICS.airTemp),
+                "TRACK_ROAD_TEMPERATURE":str(PHYSICS.roadTemp),
+                "TRACK_WIND_SPEED":str(GRAPHICS.windSpeed),
+                "TRACK_WIND_DIRECTION":str(GRAPHICS.windDirection),
+                "TRACK_SURFACE_GRIP":str(GRAPHICS.surfaceGrip),
+            },
+        }
+        #send_data(track_data, IS_RACING)
+
+        car_data = {
+            "METADATA":{
+                "TOKEN":CONFIG_OBJECT['AUTH']['token']
+            },
+            "CAR_INFO":{
+                "CAR_MODEL":str(STATIC.carModel),
+                "CAR_SPONSER":str(STATIC.carSkin),
+                "CAR_SPEEDKMH":str(PHYSICS.speedKmh),
+                "CAR_RPM":str(PHYSICS.rpms),
+                "CAR_GEAR":str(PHYSICS.gear - 1),
+                "CAR_GAS_PEDAL":str(PHYSICS.gas),
+                "CAR_BRAKE_PEDAL":str(PHYSICS.brake),
+                "CAR_CLUTCH_PEDAL":str(PHYSICS.clutch),
+                "CAR_STEER_ANGLE":str(PHYSICS.steerAngle),
+                "CAR_TYRE_COMPOUND":str(GRAPHICS.tyreCompound)
             }
+        }
+        #send_data(car_data, IS_RACING)
 
-            sendData(data, FIRST_RUN)
+        time_data = {
+            "METADATA":{
+                "TOKEN":CONFIG_OBJECT['AUTH']['token']
+            },
+            "TIME_INFO":{
+                "TIME_CURRENT_TIME":str(time),
+                "TIME_BEST_TIME":str(best_lap)
+            }
+        }
+        #send_data(time_data, IS_RACING)
 
-def acShutdown():
-    sendData(DISCONNECT_MESSAGE, False)
+#def acShutdown():
+    #send_data(DISCONNECT_MESSAGE, False)
